f7dc1394bcc53e4ca66feda4aa76da75e5c3ce7d
Steven Tang <767527493@qq.com>
2018-12-19 23:21:03 +0800

    8 update

--- Solutions/8/src/Solution.java
+++ Solutions/8/src/Solution.java
@@ -1,41 +1,36 @@
+import java.net.Inet4Address;
+
 public class Solution {
     public static int myAtoi(String str) {
-//        if(str.equals("")){
-//            return 0;
-//        }
         if(str.isEmpty()) return 0;
-        long re = 0;
-        while (!str.equals("") && str.charAt(0) == ' '){
-            str = str.substring(1);
-        }
+        long result = 0;
+        str = str.trim();
         if(str.isEmpty()) return 0;
-        Boolean isNegative = false;
+        boolean isNegative = false;
         if (str.charAt(0) == '-' || str.charAt(0) == '+'){
             if(str.charAt(0) == '-') isNegative = true;
-            else isNegative = false;
             str = str.substring(1);
         }
         for(int i = 0; i <str.length();i++) {
             if(str.charAt(i)<'0' || str.charAt(i)>'9'){
                 break;
             }
-            re = re * 10 + (str.charAt(i) - '0');
-            if(re > 2147483647 && isNegative == false){
-                return 2147483647;
+            result = result * 10 + (str.charAt(i) - '0');
+            if(result > Integer.MAX_VALUE && !isNegative){
+                return Integer.MAX_VALUE;
             }
-            if(-re < -2147483648 && isNegative == true){
-                return -2147483648;
+            if(-result < Integer.MIN_VALUE && isNegative){
+                return Integer.MIN_VALUE;
             }
         }
         if (isNegative) {
-            re = -re;
+            result = -result;
         }
-        int ret = (int) re;
-        return ret;
+        return (int) result;
     }
     public static void main(String[] args) {
         System.out.println(myAtoi("1223a"));
-        System.out.println(myAtoi("-1223"));
+        System.out.println(myAtoi("  -1223"));
         System.out.println(myAtoi("   "));
         System.out.println(myAtoi("-+1"));
         //System.out.println(Integer.parseInt("1223"));
