43a597294e8cabcbf04e3625f088380f0fa83b91
Steven Tang <767527493@qq.com>
2019-02-05 10:04:32 -0800

    46

--- Solutions/46/src/Solution.java
+++ Solutions/46/src/Solution.java
@@ -4,29 +4,52 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
-    public List<List<Integer>> permute(int[] nums){
-        if (nums.length == 0) return null;
-        List<List<Integer>> l1_1 = new ArrayList<>();
-        List<Integer> l1_2 = new ArrayList<>();
-        l1_2.add(nums[0]);
-        l1_1.add(l1_2);
-        if(nums.length == 1) return l1_1;
-        List<List<Integer>> recent_result = permute(Arrays.copyOfRange(nums, 1,nums.length));
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i =0; i<recent_result.size(); i++){
-            List<Integer> recent_list = recent_result.get(i);
-            for(int j = 0; j <= recent_list.size(); j++){
-                List<Integer> new_list = new ArrayList<>(recent_list);
-                new_list.add(j,nums[0]);
-                result.add(new_list);
-            }
-        }
-        return result;
-    }
-
     private List<List<Integer>> result;
     private boolean[] used;
-    public List<List<Integer>> permute2(int[] nums){
+
+    /**
+     * The main function for testing.
+     */
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        List<List<Integer>> result = new Solution().permute2(a);
+        for (int i = 0; i < result.size(); i++) {
+            List<Integer> recent = result.get(i);
+            System.out.println(recent);
+        }
+    }
+
+    /**
+     * The first version of permute.
+     */
+    public List<List<Integer>> permute(final int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        List<List<Integer>> listOne = new ArrayList<>();
+        List<Integer> listTwo = new ArrayList<>();
+        listTwo.add(nums[0]);
+        listOne.add(listTwo);
+        if (nums.length == 1) {
+            return listOne;
+        }
+        List<List<Integer>> recentResult = permute(Arrays.copyOfRange(nums, 1, nums.length));
+        List<List<Integer>> resultPermute = new ArrayList<>();
+        for (int i = 0; i < recentResult.size(); i++) {
+            List<Integer> recentList = recentResult.get(i);
+            for (int j = 0; j <= recentList.size(); j++) {
+                List<Integer> newList = new ArrayList<>(recentList);
+                newList.add(j, nums[0]);
+                resultPermute.add(newList);
+            }
+        }
+        return resultPermute;
+    }
+
+    /**
+     * The second version using recursive ways.
+     */
+    public List<List<Integer>> permute2(int[] nums) {
         result = new ArrayList<>();
         if (nums != null && nums.length != 0) {
             used = new boolean[nums.length];
@@ -35,7 +58,7 @@ public class Solution {
         return result;
     }
 
-    private void generatePermutation(int[] nums, int index, LinkedList<Integer> recent){
+    private void generatePermutation(int[] nums, int index, List<Integer> recent) {
         if (index == nums.length) {
             result.add(new LinkedList<>(recent));
             // Or use this :
@@ -44,21 +67,12 @@ public class Solution {
         }
         for (int i = 0; i < nums.length; i++) {
             if (!used[i]) {
-                recent.addLast(nums[i]);
+                recent.add(nums[i]);
                 used[i] = true;
                 generatePermutation(nums, index + 1, recent);
-                recent.removeLast();
+                recent.remove(recent.size() - 1);
                 used[i] = false;
             }
-        }
-    }
-
-    public static void main(String[] args){
-        int[] a = {1,2,3};
-        List<List<Integer>> result = new Solution().permute2(a);
-        for (int i =0; i<result.size(); i++) {
-            List<Integer> recent = result.get(i);
-            System.out.println(recent);
         }
     }
 }
