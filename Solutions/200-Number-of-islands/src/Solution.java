101799eedb6169d584aa51d0b12e34b3988872c1
Steven Tang <767527493@qq.com>
2019-02-20 11:58:25 -0800

    200 & 819

---
+++ Solutions/200-Number-of-islands/src/Solution.java
@@ -0,0 +1,158 @@
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Solution {
+    private int height, width;
+    private boolean[][] isVisited;
+    public int numIslands(char[][] grid) {
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int num = 0;
+        height = grid.length;
+        if (height == 0) {
+            return num;
+        }
+        width = grid[0].length;
+        isVisited = new boolean[height][width];
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] == '1' && !isVisited[i][j]) {
+                    queue.add(new Pair<>(i, j));
+                    while (!queue.isEmpty()) {
+                        Pair<Integer, Integer> point = queue.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int x = point.getKey() + directions[k][0];
+                            int y = point.getValue() + directions[k][1];
+                            if (isValid(x, y) && grid[x][y] == '1') {
+                                queue.add(new Pair<>(x, y));
+                                isVisited[x][y] = true;
+                            }
+                        }
+                    }
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+
+    private boolean isValid(int i, int j) {
+        return (i >= 0 && i < height && j >= 0 && j < width && !isVisited[i][j]);
+    }
+
+    public int numIslands2(char[][] grid) {
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int num = 0;
+        height = grid.length;
+        if (height == 0) {
+            return num;
+        }
+        width = grid[0].length;
+        isVisited = new boolean[height][width];
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] == '1' && !isVisited[i][j]) {
+                    queue.add(new int[]{i, j});
+                    while (!queue.isEmpty()) {
+                        int[] point = queue.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int x = point[0] + directions[k][0];
+                            int y = point[1] + directions[k][1];
+                            if (isValid(x, y) && grid[x][y] == '1') {
+                                queue.add(new int[]{x, y});
+                                isVisited[x][y] = true;
+                            }
+                        }
+                    }
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+
+    // DFS
+    public int numIslands3(char[][] grid) {
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int num = 0;
+        height = grid.length;
+        if (height == 0) {
+            return num;
+        }
+        width = grid[0].length;
+        Stack<int[]> stack = new Stack<>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] == '1') {
+                    stack.push(new int[]{i, j});
+                    while (!stack.isEmpty()) {
+                        int[] point = stack.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int x = point[0] + directions[k][0];
+                            int y = point[1] + directions[k][1];
+                            if ((x >= 0 && x < height && y >= 0 && y < width) && grid[x][y] == '1') {
+                                stack.add(new int[]{x, y});
+                                grid[x][y] = '0';
+                            }
+                        }
+                    }
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+
+    private char[][] globalGrid;
+    public int numIslands4(char[][] grid) {
+        globalGrid = grid;
+        int num = 0;
+        height = grid.length;
+        if (height == 0) {
+            return num;
+        }
+        width = grid[0].length;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (globalGrid[i][j] == '1') {
+                    dfs2(i, j);
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+
+    private void dfs(int i, int j) {
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int k = 0; k < 4; k++) {
+            int x = i + directions[k][0];
+            int y = j + directions[k][1];
+            if ((x >= 0 && x < height && y >= 0 && y < width) && globalGrid[x][y] == '1') {
+                globalGrid[x][y] = '0';
+                dfs(x, y);
+            }
+        }
+    }
+
+    // Faster
+    private void dfs2(int i, int j) {
+        if (i < 0 || i > height - 1 || j < 0 || j > width - 1 || globalGrid[i][j] == '0') {
+            return;
+        }
+        globalGrid[i][j] = '0';
+        dfs2(i - 1, j);
+        dfs2(i + 1, j);
+        dfs2(i, j - 1);
+        dfs2(i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
+        System.out.println(new Solution().numIslands4(grid));
+    }
+}
---
+++ Solutions/819-Most-common-word/src/Solution.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Solution {
+    class MyComparator implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> one, Map.Entry<String, Integer> two) {
+            return two.getValue() - one.getValue();
+        }
+    }
+
+    // Count and sort, Time O(nlogn)
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph += '.';
+        Set<String> set = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> map = new HashMap<>();
+        StringBuilder word = new StringBuilder();
+        for (char c: paragraph.toCharArray()) {
+            if (Character.isLetter(c)) {
+                word.append(c);
+            } else if (word.length() > 0) {
+                String wordLow = word.toString().toLowerCase();
+                if (!set.contains(wordLow)) {
+                    map.put(wordLow, map.getOrDefault(wordLow, 0) + 1);
+                }
+                word = new StringBuilder();
+            }
+        }
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+        Collections.sort(entryList, new MyComparator());
+        return entryList.get(0).getKey();
+    }
+
+    // Count, Time O(n), or more specific O(P + B)
+    public String mostCommonWord2(String paragraph, String[] banned) {
+        paragraph += '.';
+        Set<String> set = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> map = new HashMap<>();
+        StringBuilder word = new StringBuilder();
+        int resultNum = 0;
+        String result = null;
+        for (char c: paragraph.toCharArray()) {
+            if (Character.isLetter(c)) {
+                word.append(c);
+            } else if (word.length() > 0) {
+                String wordLow = word.toString().toLowerCase();
+                if (!set.contains(wordLow)) {
+                    map.put(wordLow, map.getOrDefault(wordLow, 0) + 1);
+                    if (map.get(wordLow) > resultNum) {
+                        resultNum = map.get(wordLow);
+                        result = wordLow;
+                    }
+                }
+                word = new StringBuilder();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String paragraph = "Bob hit a 'ball', the hit BALL flew far after it was hit.";
+        String[] banned = {"hit"};
+        System.out.println(new Solution().mostCommonWord2(paragraph, banned));
+    }
+}
