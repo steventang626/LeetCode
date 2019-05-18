4d8e095f233d072d98784c2dbd3feb8c933813a6
Steven Tang <767527493@qq.com>
2019-05-18 15:58:23 -0700

    40 new

--- Solutions/40/src/Solution.java
+++ Solutions/40/src/Solution.java
@@ -2,7 +2,9 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
@@ -107,10 +109,42 @@ public class Solution {
         }
         return result;
     }
+
+    private List<List<Integer>> combinationSumResult;
+    public List<List<Integer>> combinationSum2_2(int[] candidates, int target) {
+        combinationSumResult = new ArrayList<>();
+        if (candidates.length <= 0 || target <= 0) {
+            return combinationSumResult;
+        }
+        Arrays.sort(candidates);
+        generateCombinationSum(candidates, target, 0, new ArrayList<>());
+        return combinationSumResult;
+    }
+
+    private void generateCombinationSum(int[] candidates, int target, int start, List<Integer> recentResult) {
+        if (target == 0) {
+            combinationSumResult.add(new ArrayList<>(recentResult));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            // Pruning
+            if (candidates[i] > target) {
+                return;
+            }
+            // Avoid duplicate results
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            recentResult.add(candidates[i]);
+            generateCombinationSum(candidates, target - candidates[i], i + 1, recentResult);
+            recentResult.remove(recentResult.size() - 1);
+        }
+    }
+
     public static void main(String[] args){
         int[] a = {2,5,2,1,2};
         int target = 5;
-        List<List<Integer>> result = new Solution().combinationSum2(a,target);
+        List<List<Integer>> result = new Solution().combinationSum2_2(a,target);
         //System.out.print(result.size());
         for(int i = 0; i<result.size();i++){
             List<Integer> r = result.get(i);
