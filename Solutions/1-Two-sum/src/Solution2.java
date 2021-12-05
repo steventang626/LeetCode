b70f93e8246bb33883b034928bd2a7d1eb9b6de2
Yuhan Tang <yh-ta14@qq.com>
2021-12-04 22:20:31 -0800

    Update problem 1

---
+++ Solutions/1-Two-sum/src/Solution2.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2 {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numberMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numberMap.containsKey(target - nums[i])) {
+                return new int[]{numberMap.get(target - nums[i]), i};
+            }
+            numberMap.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 4};
+        int target = 6;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+
+    }
+
+}
